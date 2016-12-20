821aae3c106e25769d5ab9367b84d7cf6c28f2bf
Felix Rosén <felix.m.rosen@gmail.com>
2016-12-20 17:33:57 +0100

    Added a token that will expire 18/02/2017. And a little bit of "clean up"

--- parser.py
+++ parser.py
@@ -1,60 +1,63 @@
 import facebook
 import json
+import sys
 from prettytable import PrettyTable
 from operator import itemgetter
 from collections import OrderedDict
 
-token = 'EAACEdEose0cBALRZCzZCk7FjjXHVBNxxZAfc4pEeW0mHBi5vcx4yPVgWxM0JZCgtkVkghHNJk7dzDWHsw7aLAEYowwwGnzUYRbo54CiEBZBXw3bLpTGuiUDVBSmWUtoB6BZCjKo0KvXtNkKZCWKU3Qkv3TrCsJfytOilwLmIF3xRJAmT3czKtIF'
+def link_parser(link):
+	id_list = [] # [user_id, post_id]
+	#extract the path
+	link_split = link.split('/')
+	for index, elem in enumerate(link_split):
+		if elem.startswith('www.'):
+			id_list.append(link_split[index+1])
+		if len(elem) == 16 and elem.isdigit():
+			id_list.append(elem)
+	return id_list
 
+
+id_list = []
+
+if sys.argv[1] == 'test':
+	#https://www.facebook.com/teknikpalatset/photos/1747691832217338/
+	id_list = ["1410055755980949", "1747691832217338"]
+else:
+	id_list = link_parser(sys.argv[1])
+
+#  will expire on 18 February 2017:
+token = 'EAAZAmF9LHNgQBADsT1IERZAVb3D9gk0ty4X5G1SOMfE9Ef33xMG0BVidteZAtczKflGAu2oxmbm4XOm98uqzMROhHx6LxoL4ilJZA3EfvnzMqpgssW9BY9sPucoSqE1IceBrPhA3Y7JZA6LQzz1pnZAh2gbrmOaLoZD'
 graph = facebook.GraphAPI(token)
-profile = graph.get_object("me")
-print(profile)
-user_id2 = '1410055755980949'
-post_id2 = '1747691832217338'
-#user_id = profile['id']
-#post_id = ?!
-
-post_data = graph.get_object(user_id2+"_"+post_id2+"/comments?limit=800") # change limit depending on the amount of comments (max 5000 i think)
+user_id = graph.get_object(id_list[0])['id']
+post_id = id_list[1]
+post_data = graph.get_object(user_id+"_"+post_id+"/comments?limit=800") # change limit depending on the amount of comments (max 5000 i think)
 
 numbers = []
-
-# TODO: A parser class to find and extract number from a sentence like: "the answer is 60".
-
 comments = 0
 # get each message from the dictionary
 for entry in post_data['data']:
-	# when strParser class is done. Call it here on each iteriation on entry
-	#comment was read
 	comments += 1
 	try:
-		# if convertable to an int
-		# typecast to int and add to list
 		numbers.append(int(entry['message'][:2]))
 	except ValueError:
 		pass
 
 # create dictionary for how many times a number appears.
-numbers_dict = {'Only one': 0}
+numbers_dict = {}
 for i in numbers:
 	if numbers.count(i) > 1:
 		numbers_dict[i] = numbers.count(i)
-	else:
-		numbers_dict['Only one'] += 1
+	elif 'Only one' not in numbers_dict.keys():
+		numbers_dict['Only one'] = 1
+	else: numbers_dict['Only one'] += 1
 
-#sort the dictionary after values
 sorted_numbers_dict = OrderedDict(sorted(numbers_dict.items(), key=itemgetter(1), reverse=True))
 
-# percentage of parsed comments that a number was extraced from
-percentage = len(numbers)/comments
-print("{} success rate". format(percentage))
-print("numbers: {}".format(len(numbers)))
 print("comments: {}".format(comments))	
+print("{} success rate". format(len(numbers)/comments)) # percentage of parsed comments that a number was extraced from
 
-#initiating table
 table = PrettyTable(["Number", "# of answers", "'%' of answers"])
-
-# add all values to the table
-for key,val in sorted_numbers_dict.items():
+for key,val in sorted_numbers_dict.items(): # add all values to the table
 	n = val/len(numbers)
 	table.add_row([key, val, round(n,3)])
 
